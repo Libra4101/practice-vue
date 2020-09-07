7a6678a061daa2da23671107752086647c492ff3
Libra4101 <satoru98ki@gmail.com>
2020-09-07 19:41:36 +0900

    :+1: Vue computed

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@
 new Vue({
   el: '#app',
   data: {
-    message: "hello"
+    message: "hello",
+    count: 0
+  },
+  computed: {
+    lessThanThree: function() {
+      return this.count > 5 ? "5より上" : "5より下"
+    }
   }
 })
