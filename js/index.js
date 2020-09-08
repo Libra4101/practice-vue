254beb862b5abe8050bf07e8a9b30140bbcc49f9
Libra4101 <satoru98ki@gmail.com>
2020-09-08 20:56:35 +0900

    :sparkles: Add v-if v-on

--- js/index.js
+++ js/index.js
@@ -3,11 +3,17 @@ new Vue({
   el: '#app',
   data: {
     message: "hello",
-    count: 0
+    count: 0,
+    fruits: ['りんご', 'バナナ', 'ぶどう']
   },
   computed: {
     lessThanThree: function() {
       return this.count > 5 ? "5より上" : "5より下"
+    }
+  },
+  methods: {
+    joinFruits: function() {
+      document.getElementById('fruitList').textContent = this.fruits.join(',');
     }
   }
 })
